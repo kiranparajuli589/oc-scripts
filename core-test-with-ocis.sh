e91ddf117951507c4d5a2126743db375aa30dea9
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-06-24 18:30:32 +0545

    added expected failures env

--- core-test-with-ocis.sh
+++ core-test-with-ocis.sh
@@ -6,10 +6,12 @@ export TEST_OCIS=true
 export STORAGE_DRIVER=$DRIVER
 if [ $DRIVER == 'owncloud' ]
 then
-	export OCIS_REVA_DATA_ROOT=/srv/app/tmp/ocis/owncloud/data/
+	export EXPECTED_FAILURES_FILE=$HOME/go/src/github.com/owncloud/ocis/tests/acceptance/expected-failures-API-on-OWNCLOUD-storage.md
+	export OCIS_REVA_DATA_ROOT=/tmp/ocis/owncloud/data/
 	export OCIS_SKELETON_STRATEGY=copy
 	export BEHAT_FILTER_TAGS=~@skipOnOcis&&~@notToImplementOnOCIS&&~@toImplementOnOCIS&&~comments-app-required&&~@federation-app-required&&~@notifications-app-required&&~systemtags-app-required&&~@local_storage&&~@skipOnOcis-owncloud-Storage
 else
+	export EXPECTED_FAILURES_FILE=$HOME/go/src/github.com/owncloud/ocis/tests/acceptance/expected-failures-API-on-OCIS-storage.md
 	export OCIS_REVA_DATA_ROOT=
 	export OCIS_SKELETON_STRATEGY=upload
 	export BEHAT_FILTER_TAGS=~@skipOnOcis&&~@notToImplementOnOCIS&&~@toImplementOnOCIS&&~comments-app-required&&~@federation-app-required&&~@notifications-app-required&&~systemtags-app-required&&~@local_storage&&~@skipOnOcis-OCIS-Storage
