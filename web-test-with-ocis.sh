e5647e69f46c5574355ea7ab0d932c4e00dedd1b
Parajuli Kiran <kiranparajuli589@gmail.com>
2021-07-01 14:11:36 +0545

    Added script to run web test with ocis backend

---
+++ web-test-with-ocis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+export SERVER_HOST="https://localhost:9200"
+export BACKEND_HOST="https://localhost:9200"
+export OCIS_REVA_DATA_ROOT="/tmp/ocis/owncloud/data"
+export TESTING_DATA_DIR="/tmp/testing/data"
+export WEB_UI_CONFIG="$HOME/www/ocConfigs/ocis-config/ocis-config.json"
+export LOCAL_UPLOAD_DIR="/uploads"
+
+cd $HOME/www/web
+
+yarn run test:acceptance:ocis "$1"
