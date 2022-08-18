53e948766761e5b682beab024eac1341af187cca
Kiran Parajuli <kiranparajuli589@gmail.com>
2022-08-18 16:18:21 +0545

    Update web selenium image and add support for chrome and firefox browsers
    Signed-off-by: Kiran Parajuli <kiranparajuli589@gmail.com>

--- services/web_selenium.sh
+++ services/web_selenium.sh
@@ -1,8 +1,29 @@
 #!/bin/bash
 
+BROWSER=$1
+
+# if empty $1, then use default browser
+if [ -z "$BROWSER" ]; then
+	BROWSER="chrome"
+fi
+
+if [ "$BROWSER" != "chrome" ] && [ "$BROWSER" != "firefox" ]; then
+	echo "Please provide a browser option: chrome or firefox"
+	exit 1
+fi
+
+SELENIUM_STANDALONE_CHROME="selenium/standalone-chrome:104.0-20220812"
+SELENIUM_STANDALONE_FIREFOX="selenium/standalone-firefox:104.0-20220812"
+
+if [ "$BROWSER" == "chrome" ]; then
+	SELENIUM=$SELENIUM_STANDALONE_CHROME
+elif [ "$BROWSER" == "firefox" ]; then
+	SELENIUM=$SELENIUM_STANDALONE_FIREFOX
+fi
+
 docker run --rm -d \
 	--network="host" \
 	-v /dev/shm:/dev/shm \
 	-v "${REMOTE_UPLOAD_DIR:-$HOME/www/owncloud/middleware/filesForUpload}":"${LOCAL_UPLOAD_DIR:-/uploads}":ro \
 	--name web-tests-selenium \
-	selenium/standalone-chrome-debug:3.141.59
+	"$SELENIUM"
